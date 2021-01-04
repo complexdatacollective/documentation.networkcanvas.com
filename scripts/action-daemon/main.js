ea47d23e072386b5f523d44dd0c7f24ae10b4000
Steve Mckellar <steve@wwqrd.uk>
2021-01-04 18:20:23 +0000

    Rate limit deploy hook

--- scripts/action-daemon/main.js
+++ scripts/action-daemon/main.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const path = require('path');
 const { exec } = require('child_process');
 
+const RATE_LIMIT = process.env.RATE_LIMIT || 5 * 60 * 1000; // Rate limit default to once per 5 minutes
 const PORT = process.env.PORT || 17005;
 const DEPLOY_SCRIPT = process.env.DEPLOY_SCRIPT;
 
@@ -12,29 +12,55 @@ if (!DEPLOY_SCRIPT) {
 
 console.log(`Deploy script is ${DEPLOY_SCRIPT}`);
 
+const state = {
+  running: false,
+  lastRun: new Date(0),
+};
+
+const getNextRunDate = () =>
+  new Date(state.lastRun.getTime() + RATE_LIMIT);
 
 const updateDocs = () =>
-  exec(
-    DEPLOY_SCRIPT,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    },
-  );
+  new Promise((resolve, reject) => {
+    exec(
+      DEPLOY_SCRIPT,
+      (error, stdout, stderr) => {
+        if (error) { return reject(error.message); }
+        if (stderr) { return reject(stderr); }
+        resolve(stdout);
+      },
+    );
+  });
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok\n');
-  updateDocs();
+
+  // If already running, wait
+  if (state.running) {
+    res.end('busy\n');
+    return;
+  }
+
+  const nextRunDate = getNextRunDate();
+
+  // If run recently, wait
+  if (state.lastRun !== null && Date.now() < nextRunDate.getTime()) {
+    res.end(`rate limited until ${nextRunDate}\n`);
+    return;
+  }
+
+  state.running = true;
+
+  res.end(`ok`);
+
+  updateDocs()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => {
+      state.running = false;
+      state.lastRun = new Date();
+    });
 });
 
-// Start the server on PORT 3000
-app.listen(PORT, '127.0.0.1');
+app.listen(PORT, '0.0.0.0');
 console.log(`Node server running on port ${PORT}`);
